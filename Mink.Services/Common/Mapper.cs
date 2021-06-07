b3a1c601510125977d1a9f25df3ef987a3b09630
artem <horoshev.artem@yandex.ru>
2021-06-08 00:05:38 +0700

    Added 'common' folder.

---
+++ Mink.Services/Common/Mapper.cs
@@ -0,0 +1,30 @@
+﻿using Mink.Domain.Models.Dtos;
+using Mink.Domain.Models.Entities;
+
+namespace Mink.Services.Common
+{
+    public static class Mapper
+    {
+        public static MinifiedUriDto ToDto(this MinifiedUri entity)
+        {
+            return new()
+            {
+                Id = entity.Id,
+                OriginUri = entity.OriginUri,
+                MinifiedUriKey = entity.MinifiedUriKey,
+                QrImageUri = entity.QrImageUri,
+            };
+        }
+
+        public static MinifiedUri ToEntity(this MinifiedUriDto dto)
+        {
+            return new()
+            {
+                Id = dto.Id,
+                OriginUri = dto.OriginUri,
+                MinifiedUriKey = dto.MinifiedUriKey,
+                QrImageUri = dto.QrImageUri,
+            };
+        }
+    }
+}
---
+++ Mink.Services/Common/ServiceError.cs
@@ -0,0 +1,11 @@
+﻿namespace Mink.Services.Common
+{
+    public static class ServiceError
+    {
+        public static string InvalidUriFormat = "Unable to shorten that link. It is not a valid url.";
+        public static string InvalidUriDomain = "Domain not allowed.";
+        public static string InvalidUriType = "Uri not allowed.";
+        public static string UriAlreadyTaken = "Uri already taken.";
+        public static string UriKeyNotFound = "Uri not found.";
+    }
+}
